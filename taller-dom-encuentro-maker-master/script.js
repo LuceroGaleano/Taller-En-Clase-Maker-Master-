31aacbbc25808e058325a6f8d5411a56a049adc8
Lu56-prog <lucerocardona167@gmail.com>
2026-08-25 13:08:00 -0500

    Segundo feature

--- taller-dom-encuentro-maker-master/script.js
+++ taller-dom-encuentro-maker-master/script.js
@@ -58,15 +58,17 @@ campoObservaciones.maxLength = 200;
 function actualizarContador(){
   const tamañoObservaciones = campoObservaciones.value.length;
   contador.textContent = tamañoObservaciones + "/200";
+
+  //Removemos las clases del contador para poder insertar nuevas
+  contador.classList.remove("contador--alerta");
+  contador.classList.remove("contador--limite");
+
   if(tamañoObservaciones >=150 && tamañoObservaciones <200){
     contador.classList.remove("contador--limite");
     contador.classList.add("contador--alerta");
   } else if(tamañoObservaciones == 200){
     contador.classList.remove("contador--alerta");
     contador.classList.add("contador--limite");
-  }else{
-    contador.classList.remove("contador--alerta");
-    contador.classList.remove("contador--limite");
   }
 }
 
@@ -74,10 +76,52 @@ campoObservaciones.addEventListener("input", actualizarContador);
 
 
 
-
 // 📧 FEATURE 2: validación del correo electrónico
+const campoCorreo = document.getElementById("correo");
+const campoTextoValidacion = document.getElementById("validacionCorreo");
 
 
+function validarCorreoElectronico(){
+  const correoElectronico = campoCorreo.value;
+  let validador = true;
+
+  //Removemos las clases del campo para poder adiccionar
+  campoCorreo.classList.remove("campo__control--valido");
+  campoCorreo.classList.remove("campo__control--error");
+
+  const partes = correoElectronico.split("@");
+  //Validamos que si se haya dividido en dos, de lo contrario quiere decir que hay mas de un @
+  if(partes.length == 2){
+    const parteIzquierda = partes[0].trim();
+    const parteDerecha = partes[1].trim();
+    //Primero validamos que cada parte despues y antes del arroba no este vacia
+    if(parteDerecha.length === 0 || parteIzquierda.length === 0){
+      validador = false;
+    }
+    //Luego validamos que la parte derecha del arroba contenga el .
+    if(!parteDerecha.includes(".")){
+      validador = false;
+    }
+  } else{
+    validador = false;
+  }
+
+  //Validamos que el correo no este vacio
+  if(correoElectronico.length === 0){
+    campoTextoValidacion.textContent = "El correo es obligatorio";
+  } else{
+    //Por ultimo validamos segun los filtros realizados el correo esta correcto o no
+    if(validador === true){
+      campoTextoValidacion.textContent = "";
+      campoCorreo.classList.add("campo__control--valido");
+    } else{
+      campoTextoValidacion.textContent = "Escribe un correo valido, por ejemplo nombre@dominio.com";
+      campoCorreo.classList.add("campo__control--error");
+    }
+  }
+}
+
+campoCorreo.addEventListener("blur", validarCorreoElectronico);
 
 
 // 🎟️ FEATURE 3: límite de talleres seleccionables
